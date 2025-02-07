2c2e43083672c4d8485cf5dcc12803c46e037b98
Trupti16042004 <truptipardeshi91@gmail.com>
2025-02-07 10:33:52 +0530

    Calculator
    To perform Mathematical Calculations

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+class Calculator
+{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+
+		System.out.print("Enter num 1 : ");
+		float num1 = sc.nextFloat();
+
+		System.out.print("Enter num 2 : ");
+		float num2 = sc.nextFloat();
+
+
+		System.out.print("Enter operator : ");
+		char ch = sc.next().charAt(0);
+
+		float opt = 0;
+
+		opt = (op == '+')?num1+num2;
+			 (op == '-')?num1-num2;
+			 (op == '*')?num1*num2;
+			  (op == '/')?num1/num2;
+			  (op == '%')?num1%num2:0.000001f;
+
+
+		String output = num1+ " "+op+ " "+num2 +"= "   +opt;
+
+		if(!(opt == 0.00001f))
+
+
+
+	}
+}
